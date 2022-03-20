f42dc6ea2aa4fdb4cf0f77500f531e13aa29d5cb
POL6463 <kevinkim0617kim@gmail.com>
2022-03-20 22:14:43 +0900

    4-3완료

--- Project_03/app/src/main/java/com/example/project_03/MainActivity.kt
+++ Project_03/app/src/main/java/com/example/project_03/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.project_03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editT : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title="2018110006김민웅"
+
+        editT = findViewById<EditText>(R.id.EditT)
+        editT.inputType = 0
+        editT.setOnKeyListener { v, keyCode, event ->
+            if(keyCode != null) {
+                Toast.makeText(applicationContext, editT.text.toString(), Toast.LENGTH_SHORT).show()
+            }
+            false
+        }
+
     }
 }
