a324962320305d66048a3cd6e435492b1c59ee35
POL6463 <kevinkim0617kim@gmail.com>
2022-03-22 14:42:42 +0900

    Ch5

---
+++ Project5/app/src/main/java/com/example/project5/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        title="2018110006김민웅"
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
