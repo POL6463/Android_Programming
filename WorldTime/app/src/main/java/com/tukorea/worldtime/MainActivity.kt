50572fe29dd74c96ec6e3387ade0cb3459090744
POL6463 <kevinkim0617kim@gmail.com>
2022-04-10 23:35:17 +0900

    대략적인 UI완성

--- WorldTime/app/src/main/java/com/tukorea/worldtime/MainActivity.kt
+++ WorldTime/app/src/main/java/com/tukorea/worldtime/MainActivity.kt
@@ -2,10 +2,22 @@ package com.tukorea.worldtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var timeList : ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        timeList = findViewById<ListView>(R.id.timeList)
+        var mid = arrayOf("히어로즈", "24시", "호스트", "글리", "프렌즈")
+
+        var adapter : ArrayAdapter<String> = ArrayAdapter(this,
+                    android.R.layout.simple_list_item_1, mid)
+        timeList.adapter = adapter
+
     }
 }
