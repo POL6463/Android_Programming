0f0e1fd8aecd797f6b96019bf7e7a7a03d86bd6e
POL6463 <kevinkim0617kim@gmail.com>
2022-04-17 18:18:14 +0900

    임시저장

--- WorldTime/app/src/main/java/com/tukorea/worldtime/MainActivity.kt
+++ WorldTime/app/src/main/java/com/tukorea/worldtime/MainActivity.kt
@@ -1,28 +1,51 @@
 package com.tukorea.worldtime
 
+import android.content.SharedPreferences
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
+import android.preference.Preference
+import android.preference.PreferenceManager
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.google.android.material.slider.Slider
+import org.joda.time.DateTimeUtils
+import org.joda.time.DateTimeZone
+import org.w3c.dom.Text
 import java.sql.Time
+import java.time.ZoneId
+import java.time.format.TextStyle
 import java.util.*
+import java.util.prefs.Preferences
+import java.util.stream.Stream
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     lateinit var timeList : ListView
     lateinit var timeSlider : Slider
     lateinit var korTimeText : TextView
+    lateinit var testText : TextView
+    private lateinit var sharedPreferences : SharedPreferences
+    private lateinit var editor : SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+        sharedPreferences = getSharedPreferences("data", MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+
+
+
 
         timeList = findViewById<ListView>(R.id.timeList)
         timeSlider = findViewById<Slider>(R.id.timeSlider)
         korTimeText = findViewById<TextView>(R.id.korTimeText)
+        testText = findViewById<TextView>(R.id.testText)
 
         var timeSelect : Int
         var timeHour : Int
@@ -30,7 +53,27 @@ class MainActivity : AppCompatActivity() {
         var timeMin : Int
         var timeMinStr : String
         var timeForm : String
-        var timeZoneList = TimeZone.getAvailableIDs()
+
+        var locales : Array<out String> = Locale.getISOCountries()
+        for (item in locales) {
+        }
+
+
+
+        var timeZNames1 = DateTimeZone.getAvailableIDs()
+        var zoneDisplayList : ArrayList<String> = arrayListOf()
+        timeZNames1.forEach { item ->
+            zoneDisplayList.add(TimeZone.getTimeZone(item).getDisplayName(Locale.KOREA))
+            editor.putString( TimeZone.getTimeZone(item).getDisplayName(Locale.KOREA) , item )}//한국어로 타임존 디스플레이 네임 출력
+        editor.commit()
+        var zoneDisplayList1 = zoneDisplayList.distinct().sorted() //중복제거, 정렬
+
+
+
+
+
+
+
 
         var currTime = Calendar.getInstance().time
         //현재시간 받아오기
@@ -81,7 +124,7 @@ class MainActivity : AppCompatActivity() {
         var mid = arrayOf("1번", "2번", "3반", "4번", "5번")
 
         var adapter : ArrayAdapter<String> = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, timeZoneList)
+            android.R.layout.simple_list_item_1, zoneDisplayList1)
         timeList.adapter = adapter
 
 
